a1cbc6cce4be0829b079aeee6cdf94e87c0f548b
mrbrazz <mrbrazz@yandex.ru>
2021-07-13 17:30:31 +0300

    start coding main View

--- UshakovSV_HW2.3/ViewController.swift
+++ UshakovSV_HW2.3/ViewController.swift
@@ -8,12 +8,37 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var fromLabel: UILabel!
+    @IBOutlet weak var minimumValueLable: UILabel!
+    @IBOutlet weak var toLable: UILabel!
+    @IBOutlet weak var maximumValueLable: UILabel!
+    @IBOutlet weak var resultLable: UILabel!
+    @IBOutlet weak var getResultButtom: UIButton!
+    
+    let minimumValue = 0
+    let maximumValue = 100
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        getResultButtom.layer.cornerRadius = 15
+        
+        minimumValueLable.text = String(minimumValue)
+        maximumValueLable.text = String(maximumValue)
+        }
+    
+    func changeResultLable() {
+        resultLable.text = String(Int.random(in: minimumValue...maximumValue))
     }
+    
+    @IBAction func getResultAction() {
+        changeResultLable()
+    }
+    
 
+    
 
 }
 
